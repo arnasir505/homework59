4fa54d77b275cc4df5a51995aaee83ef4c2158e5
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-26 15:22:47 +0600

    Added React memo to Movie Item

--- src/Task1/MovieItem/MovieItem.tsx
+++ src/Task1/MovieItem/MovieItem.tsx
@@ -10,29 +10,29 @@ interface Props {
   removeMovie: (id: string) => void;
 }
 
-const MovieItem: React.FC<Props> = ({
-  title,
-  id,
-  handleTitleChange,
-  removeMovie,
-}) => {
-  return (
-    <div className='input-group mb-3'>
-      <input
-        type='text'
-        className='form-control fw-semibold fs-5'
-        value={title}
-        onChange={(e) => handleTitleChange(e, id)}
-      />
-      <button
-        type='button'
-        className='btn btn-outline-danger'
-        onClick={() => removeMovie(id)}
-      >
-        X
-      </button>
-    </div>
-  );
-};
+const MovieItem: React.FC<Props> = React.memo(
+  ({ title, id, handleTitleChange, removeMovie }) => {
+    return (
+      <div className='input-group mb-3'>
+        <input
+          type='text'
+          className='form-control fw-semibold fs-5'
+          value={title}
+          onChange={(e) => handleTitleChange(e, id)}
+        />
+        <button
+          type='button'
+          className='btn btn-outline-danger'
+          onClick={() => removeMovie(id)}
+        >
+          X
+        </button>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.title === nextProps.title;
+  }
+);
 
 export default MovieItem;
