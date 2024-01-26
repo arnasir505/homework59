7cc02a34c8aaba80218c7e6a62118a66ef3350a1
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-26 14:00:16 +0600

    Added functionality to add movie to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEventHandler, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import MovieForm from './Task1/MovieForm/MovieForm';
 import { Movie } from './types';
@@ -8,24 +8,31 @@ function App() {
   const [movies, setMovies] = useState<Movie[]>([
     {
       id: uuidv4(),
-      title: 'Oppenheimer'
+      title: 'Oppenheimer',
     },
     {
       id: uuidv4(),
-      title: 'Game of Thrones'
+      title: 'Game of Thrones',
     },
     {
       id: uuidv4(),
-      title: 'Star Wars'
-    }
+      title: 'Star Wars',
+    },
   ]);
+
+  const addMovie = (e: React.FormEvent<HTMLFormElement>, movie: Movie) => {
+    e.preventDefault();
+    setMovies((prevState) => [...prevState, movie]);
+  };
 
   return (
     <div className='container'>
       <div className='p-5 w-50 mx-auto'>
-        <MovieForm />
+        <MovieForm onMovieFormSubmit={addMovie}/>
         <p className='my-2'>To watch list:</p>
-        {movies.map(movie => (<MovieItem title={movie.title} id={movie.id}/>))}
+        {movies.map((movie) => (
+          <MovieItem title={movie.title} id={movie.id} key={movie.id}/>
+        ))}
       </div>
     </div>
   );
--- src/Task1/MovieForm/MovieForm.tsx
+++ src/Task1/MovieForm/MovieForm.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Movie } from '../../types';
 
-const MovieForm = () => {
+interface Props {
+  onMovieFormSubmit: (
+    e: React.FormEvent<HTMLFormElement>,
+    movie: Movie
+  ) => void;
+}
+
+const MovieForm: React.FC<Props> = ({ onMovieFormSubmit }) => {
+  const [movieInput, setMovieInput] = useState({
+    id: uuidv4(),
+    title: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMovieInput((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <form className='d-flex'>
-      <input type='text' className='form-control' required />
-      <button className='btn btn-primary ms-2'>Add</button>
+    <form className='d-flex' onSubmit={(e) => onMovieFormSubmit(e, movieInput)}>
+      <input
+        type='text'
+        className='form-control'
+        name='title'
+        value={movieInput.title}
+        onChange={handleChange}
+        required
+      />
+      <button type='submit' className='btn btn-primary ms-2'>
+        Add
+      </button>
     </form>
   );
 };
