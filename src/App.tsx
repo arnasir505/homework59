ae776bfe2de283f0e7bf2ea11e9f42d400be01ef
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-26 13:03:45 +0600

    Created movies state and movie type, installed uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import MovieForm from './Task1/MovieForm/MovieForm'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import MovieForm from './Task1/MovieForm/MovieForm';
+import { Movie } from './types';
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([
+    {
+      id: uuidv4(),
+      title: 'Oppenheimer'
+    },
+    {
+      id: uuidv4(),
+      title: 'Game of Thrones'
+    },
+    {
+      id: uuidv4(),
+      title: 'Star Wars'
+    }
+  ]);
 
   return (
-    <div className="container">
-      <div className="p-5 w-50 mx-auto">
-        <MovieForm/>
+    <div className='container'>
+      <div className='p-5 w-50 mx-auto'>
+        <MovieForm />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Task1/MovieForm/MovieForm.tsx
+++ src/Task1/MovieForm/MovieForm.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const MovieForm = () => {
   return (
     <form className='d-flex'>
-      <input type="text" className='form-control' required/>
+      <input type='text' className='form-control' required />
       <button className='btn btn-primary ms-2'>Add</button>
     </form>
-  )
-}
+  );
+};
 
-export default MovieForm
+export default MovieForm;
