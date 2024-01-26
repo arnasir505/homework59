a43f0927c5984449b96ce99ae7a8ac513f2c675a
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-26 13:35:11 +0600

    Display movies as cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import MovieForm from './Task1/MovieForm/MovieForm';
 import { Movie } from './types';
+import MovieItem from './Task1/MovieItem/MovieItem';
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([
@@ -23,6 +24,8 @@ function App() {
     <div className='container'>
       <div className='p-5 w-50 mx-auto'>
         <MovieForm />
+        <p className='my-2'>To watch list:</p>
+        {movies.map(movie => (<MovieItem title={movie.title} id={movie.id}/>))}
       </div>
     </div>
   );
---
+++ src/Task1/MovieItem/MovieItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface Props {
+  id: string
+  title: string;
+}
+
+const MovieItem: React.FC<Props> = ({ title, id }) => {
+  return (
+    <div className='card my-2'>
+      <div className='card-body d-flex justify-content-between'>
+        <h5 className='m-0'>{title}</h5>
+        <button type='button' className='btn-close'></button>
+      </div>
+    </div>
+  );
+};
+
+export default MovieItem;
